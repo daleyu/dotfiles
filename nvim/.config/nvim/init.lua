becc9eccb9b0e22f0a748446dfded285ca38b8f0
dale.yu <yudale2003@gmail.com>
2025-02-11 16:07:44 -0800

    add treesitter context instead of navic

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -95,6 +95,7 @@ require('lazy').setup({
 	{ 'echasnovski/mini.nvim', version = false },
 	{'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
 	{ "nvim-treesitter/nvim-treesitter" },
+	{ "nvim-treesitter/nvim-treesitter-context" },
 	{ "ibhagwan/fzf-lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
 	{'williamboman/mason.nvim'},
 	{'williamboman/mason-lspconfig.nvim'},
@@ -228,7 +229,9 @@ require("fzf-lua").setup({
 -- use `fzf-lua` for replace vim.ui.select 
 require("fzf-lua").register_ui_select()
 
--- Treesitter configs
+-------------------------
+--- Treesitter configs---
+-------------------------
 require("nvim-treesitter.configs").setup({
 	ensure_installed = { "cpp", "typescript", "tsx", "python", "luau", "javascript", "rust", "json", "lua", "go", "html" },
 	highlight = {
@@ -238,6 +241,21 @@ require("nvim-treesitter.configs").setup({
 		enable = true
 	},
 })
+require'treesitter-context'.setup{
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  multiwindow = false, -- Enable multiwindow support.
+  max_lines = 1, -- How many lines the window should span. Values <= 0 mean no limit.
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  line_numbers = true,
+  multiline_threshold = 1, -- Maximum number of lines to show for a single context
+  trim_scope = 'inner', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
+  zindex = 40, -- The Z-index of the context window
+  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+}
 
 ----------------
 --- Diffview ---
