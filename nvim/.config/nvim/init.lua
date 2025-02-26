51f80fdb5fa00bb0df975a55bc5a7d9b04d2c193
dale.yu <yudale2003@gmail.com>
2025-02-26 14:03:57 -0800

    remove search blocking the whole screen

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -136,7 +136,6 @@ require('lazy').setup({
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"rcarriga/nvim-notify",
@@ -451,24 +450,15 @@ vim.keymap.set("v", "<leader>ca", "<cmd>CodeSnapASCII<cr>")
 -- setup must be called before loading
 vim.cmd.colorscheme("catppuccin")
 
--- require("noice").setup({
--- 	lsp = {
--- 		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
--- 		override = {
--- 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
--- 			["vim.lsp.util.stylize_markdown"] = true,
--- 			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
--- 		},
--- 	},
--- 	presets = {
--- 		bottom_search = true,     -- use a classic bottom cmdline for search
--- 		command_palette = true,   -- position the cmdline and popupmenu together
--- 		long_message_to_split = true, -- long messages will be sent to a split
--- 		inc_rename = false,       -- enables an input dialog for inc-rename.nvim
--- 		lsp_doc_border = false,   -- add a border to hover docs and signature help
--- 	},
--- })
-
+require("noice").setup({
+	cmdline = {},
+	messages = {},
+	popupmenu = {},
+	redirect = {},
+	presets = {
+		bottom_search = true, -- use a classic bottom cmdline for search
+	},
+})
 
 ------------------------
 -- Global Keymappings---
