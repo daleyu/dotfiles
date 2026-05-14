bad38c294aec09f546eacc717993a3a914109c10
Dale Yu <yudale2003@gmail.com>
2026-05-14 16:42:14 -0700

    add the ability to copy paste from clipboard in nvim

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,6 +1,16 @@
 return {
 	{ "tpope/vim-sleuth" },
 	{
+		-- brew install pngpaste on mac
+		"HakonHarnes/img-clip.nvim",
+		opts = {
+			default = {
+				insert_mode_after_paste = false,
+				prompt_for_file_name = false,
+			},
+		},
+	},
+	{
 		"pmizio/typescript-tools.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		opts = {},
