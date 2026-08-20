9568a89360dae049030962ad881fa8734927b25b
Dale Yu <yudale2003@gmail.com>
2026-08-20 15:37:11 -0700

    remove luarc

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,18 +1,18 @@
 return {
-	{ "tpope/vim-sleuth" },
-	{
-		-- brew install pngpaste on mac
-		"HakonHarnes/img-clip.nvim",
-		opts = {
-			default = {
-				insert_mode_after_paste = false,
-				prompt_for_file_name = false,
-			},
-		},
-	},
-	{
-		"pmizio/typescript-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {},
-	},
+        { "tpope/vim-sleuth" },
+        {
+                -- brew install pngpaste on mac
+                "HakonHarnes/img-clip.nvim",
+                opts = {
+                        default = {
+                                insert_mode_after_paste = false,
+                                prompt_for_file_name = false,
+                        },
+                },
+        },
+        {
+                "pmizio/typescript-tools.nvim",
+                dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+                opts = {},
+        },
 }
