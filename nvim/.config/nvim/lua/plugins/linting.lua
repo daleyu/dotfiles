7f6bf379e3360d54c8fee24414fefd0766fff626
Dale Yu <yudale2003@gmail.com>
2026-08-20 15:15:05 -0700

    wyh is vim global not working

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -1,17 +1,18 @@
 return {
-	{
-		"mfussenegger/nvim-lint",
-		config = function()
-			local lint = require("lint")
-			lint.linters_by_ft = {
-				lua = { "selene" },
-				luau = { "selene" },
-			}
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-				callback = function()
-					lint.try_lint()
-				end,
-			})
-		end,
-	},
+        {
+                "mfussenegger/nvim-lint",
+                config = function()
+                        local lint = require("lint")
+                        lint.linters_by_ft = {
+                                lua = { "selene" },
+                                luau = { "selene" },
+                                proto = { "buf_lint" },
+                        }
+                        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                                callback = function()
+                                        lint.try_lint()
+                                end,
+                        })
+                end,
+        },
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,84 +1,103 @@
 return {
-	{
-		'neovim/nvim-lspconfig',
-		lazy = false,
-		dependencies = { "saghen/blink.cmp" },
-		config = function()
-			vim.lsp.config("*", {
-				capabilities = require("blink.cmp").get_lsp_capabilities(),
-			})
+        {
+                "neovim/nvim-lspconfig",
+                lazy = false,
+                dependencies = { "saghen/blink.cmp" },
+                config = function()
+                        vim.lsp.config("*", {
+                                capabilities = require("blink.cmp").get_lsp_capabilities(),
+                        })
 
-			local servers = {
-				"rust_analyzer", "bashls",
-				"jsonls", "ts_ls", "zls", "eslint", "thriftls",
-				"buf_ls", "tinymist", "yamlls"
-			}
+                        local servers = {
+                                "rust_analyzer",
+                                "bashls",
+                                "jsonls",
+                                "ts_ls",
+                                "zls",
+                                "eslint",
+                                "thriftls",
+                                "buf_ls",
+                                "tinymist",
+                                "yamlls",
+                                "lua_ls",
+                        }
 
-			for _, server in ipairs(servers) do
-				vim.lsp.enable(server)
-			end
+                        for _, server in ipairs(servers) do
+                                vim.lsp.enable(server)
+                        end
 
-			vim.diagnostic.config({
-				float = {
-					border = "rounded",
-					source = true,
-				},
-			})
-		end,
-	},
-	{ 'williamboman/mason.nvim' },
-	{
-		'williamboman/mason-lspconfig.nvim',
-		config = function()
-			require('mason').setup({})
-			vim.lsp.config('lua_ls', {
-				settings = {
-					Lua = {
-						runtime = {
-							version = "LuaJIT",
-						},
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							checkThirdParty = false,
-							library = vim.api.nvim_get_runtime_file("lua", true),
-						},
-					},
-				},
-			})
-			vim.lsp.config('pyright', {
-				settings = {
-					pyright = {
-						disableOrganizeImports = true,
-					},
-					python = {
-						analysis = {
-							ignore = { "*" },
-						},
-					},
-				},
-			})
-			require('mason-lspconfig').setup({
-				ensure_installed = {
-					"lua_ls", "rust_analyzer", "bashls",
-					"jsonls", "ts_ls", "zls", "eslint", "thriftls",
-					"buf_ls", "marksman", "tinymist"
-				},
-			})
-		end,
-	},
-	{
-		"j-hui/fidget.nvim",
-		opts = { notification = { window = { winblend = 0 } } },
-	},
-	{
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			},
-		},
-	},
+                        vim.diagnostic.config({
+                                float = {
+                                        border = "rounded",
+                                        source = true,
+                                },
+                        })
+                end,
+        },
+        { "williamboman/mason.nvim" },
+        {
+                "williamboman/mason-lspconfig.nvim",
+                config = function()
+                        require("mason").setup({})
+                        vim.lsp.config("lua_ls", {
+                                settings = {
+                                        Lua = {
+                                                runtime = {
+                                                        version = "LuaJIT",
+                                                },
+                                                diagnostics = {
+                                                        globals = { "vim" },
+                                                },
+                                                workspace = {
+                                                        checkThirdParty = false,
+                                                        library = vim.api.nvim_get_runtime_file(
+                                                                "lua",
+                                                                true
+                                                        ),
+                                                },
+                                        },
+                                },
+                        })
+                        vim.lsp.config("pyright", {
+                                settings = {
+                                        pyright = {
+                                                disableOrganizeImports = true,
+                                        },
+                                        python = {
+                                                analysis = {
+                                                        ignore = { "*" },
+                                                },
+                                        },
+                                },
+                        })
+                        require("mason-lspconfig").setup({
+                                ensure_installed = {
+                                        "lua_ls",
+                                        "rust_analyzer",
+                                        "bashls",
+                                        "jsonls",
+                                        "ts_ls",
+                                        "zls",
+                                        "eslint",
+                                        "thriftls",
+                                        "buf_ls",
+                                        "marksman",
+                                        "tinymist",
+                                },
+                        })
+                end,
+        },
+        {
+                "j-hui/fidget.nvim",
+                opts = { notification = { window = { winblend = 0 } } },
+        },
+        {
+                "folke/lazydev.nvim",
+                ft = "lua",
+                opts = {
+                        library = {
+                                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                        },
+                },
+        },
 }
